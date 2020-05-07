d236708bbaf272487538e72a25a4466058da34f0
田兵 <tianbingb@yonyou.com>
2020-05-07 18:43:23 +0800

    Swagger2

--- demo/src/main/java/com/tb/aop/demo/aop/Test.java
+++ demo/src/main/java/com/tb/aop/demo/aop/Test.java
@@ -1,5 +1,10 @@
 package com.tb.aop.demo.aop;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiImplicitParams;
+import io.swagger.annotations.ApiOperation;
+import org.apiguardian.api.API;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 
@@ -10,10 +15,15 @@ import org.springframework.web.bind.annotation.*;
  */
 @RestController
 @RequestMapping("/Test")
-@Transactional(rollbackFor = Exception.class)
+@Api(value = "测试")
 public class Test {
 
     @MyLog(desc = "测试aop")
+    @ApiOperation(value="aop测试", notes="aop测试")
+    @ApiImplicitParams({
+            @ApiImplicitParam(paramType="query", name = "a", value = "a", required = true, dataType = "String"),
+            @ApiImplicitParam(paramType="query", name = "b", value = "b", required = true, dataType = "String")
+    })
     @GetMapping(value = "/aopTest")
     public void Test(String a,String b){
 
